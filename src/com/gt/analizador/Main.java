6a5eecac2db10e12ae0aaa249e31dd303c3346b1
7eo Aldamas <leo.aldamas@gmail.com>
2021-08-04 22:19:49 -0600

    Interfaz gráfica - Principal | Imagenes

--- src/com/gt/analizador/Main.java
+++ src/com/gt/analizador/Main.java
@@ -1,9 +1,16 @@
 package com.gt.analizador;
 
+import com.gt.vista.GUI.Principal;
+import java.awt.EventQueue;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Inicio Git");
+        System.out.println("Inicio Git");       
+        
+        EventQueue.invokeLater(() -> {
+            new Principal().setVisible(true);
+        });
     }
     
 }
